405a7056251206d0c07e256eaea2debc449e2399
Tom Black <tom.black06007@gmail.com>
2021-04-17 22:18:39 -0700

    Fixed an issue where it wasn't calculating correctly.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,44 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(isLeapYear(2024));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter any year to determine if it's a leap year or not.");
+        int userYear = scanner.nextInt();
+        scanner.close();
+        isLeapYear(userYear);
+
     }
 
-    public static boolean isLeapYear(int year) {
-        if(year < 1 && year > 9999) {
-            return false;
+    public static void isLeapYear(int year) {
+        boolean isLeap = false;
+        if(year < 1 || year > 9999) {
+            isLeap = false;
+            System.out.println(year + " is not a leap year.");
+            return;
         }
 
         if(year % 4 == 0) {
             if(year % 100 == 0) {
-                if(year % 400 == 0) {
-                    return true;
-                } else {
-                    return false;
-                }
-            } else if(year % 400 == 0) {
-                return true;
+                if(year % 400 == 0)
+                    isLeap = true;
+                else
+                    isLeap = false;
             }
+            else
+                isLeap = true;
+        } else {
+            isLeap = false;
         }
-        return false;
+
+        if(isLeap = true) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " isn't a leap year.");
+        }
     }
 
 }
